43e3d6e78c79ba51d580645b577d83f03e8ed581
Farzad YZ <farskid@gmail.com>
2019-05-15 21:15:44 +0300

    remove default rootElement from pa11y runner

--- lib/runners/pa11y.ts
+++ lib/runners/pa11y.ts
@@ -3,7 +3,6 @@ import { Report, Runner, Pa11yOptions } from "../types";
 
 export const runWithPa11y: Runner = async (url: string) => {
   const pa11yOptions: Partial<Pa11yOptions> = {
-    rootElement: "#__next",
     standard: "WCAG2A",
     includeNotices: true,
     includeWarnings: true
